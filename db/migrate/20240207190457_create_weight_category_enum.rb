e82bc9362dff3e0778b9c0a05e4babfcdfab9ccb
dominik <dominik.alberski@gmail.com>
2024-02-08 21:03:47 +0100

    fix: add missing category to enum

--- db/migrate/20240207190457_create_weight_category_enum.rb
+++ db/migrate/20240207190457_create_weight_category_enum.rb
@@ -1,7 +1,7 @@
 class CreateWeightCategoryEnum < ActiveRecord::Migration[7.1]
   def up
     execute <<-SQL
-      CREATE TYPE weight_category AS ENUM ('oz_1', 'oz_1_2', 'oz_1_4', 'oz_1_10', 'oz_1_20');
+      CREATE TYPE weight_category AS ENUM ('oz_1', 'oz_1_2', 'oz_1_4', 'oz_1_10', 'oz_1_20', 'other');
     SQL
   end
 
